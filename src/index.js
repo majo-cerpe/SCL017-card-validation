34c9680805497b46fd083ba8074e1910168efd53
majo-cerpe <mariajose_cerpe@outlook.com>
2021-05-30 21:19:05 -0400

    My First Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+document.getElementById("creditCardNumber").onkeyup = validator.maskify;
+
+
+document.getElementById("validationButton").onclick = validator.isValid;
+
+
+/*
+
+document.getElementById("validationButton")
+    .addEventListener("click", function () {
+        let creditCardNumber = document.getElementById("creditCardNumber").value;
+
+        if (creditCardNumber == "") {
+            alert("ingresa tu numero")
+        } else {
+            document.getElementById("validationButton").onclick = validator.isValid;
+        }
+    });
+    
+*/
