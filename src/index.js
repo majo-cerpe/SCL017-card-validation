4c12d5d753d9f68bb0cededc0c86228440764168
majo-cerpe <mariajose_cerpe@outlook.com>
2021-06-02 11:41:08 -0400

    Proyect Feedback Commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import validator from './validator.js';
 
 console.log(validator);
 
-
+document.getElementById("button1")
+    .addEventListener("click", function () {
+        document.getElementById("firstPanel").hidden = true;
+        document.getElementById("secondPanel").hidden = false;
+    });
 
 document.getElementById("creditCardNumber").onkeyup = validator.maskify;
-
 
 document.getElementById("validationButton")
     .addEventListener("click", function () {
@@ -13,9 +16,19 @@ document.getElementById("validationButton")
         if (document.getElementById("creditCardNumber").value == "") {
             alert("debes ingresar un número de tarjeta de crédito válido")
         } else if (validationResult == true) {
-            alert("El número ingresado es válido")
+            alert("Los datos ingresados son válidos")
+            document.getElementById("secondPanel").hidden = true;
+            document.getElementById("thirdPanel").hidden = false;
         } else {
             alert("el número ingresado es inválido")
         }
     });
+
+    document.getElementById("confirmPurchaseButton")
+    .addEventListener("click", function () {
+        alert("¿Estás seguro de confirmar tu compra?")
+        document.getElementById("thirdPanel").hidden = true;
+        document.getElementById("fourthPanel").hidden = false;
+        });
+
 
